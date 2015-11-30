21d7f710283acb93e864a350bba9a27636b55873
ahamed07sajeeb <sajeebo07ahamed@gmail.com>
2015-11-30 22:58:46 +0600

    this is my first commit about mohangonj pilot govt high school

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Mohangonj Pilot Govt. High School</title>
+        
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/mystyle.css">
+        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/bootstrap-social.css">
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    </head>
+    <body>
+        
+        
+        <!-- include the menu bar-->
+        <?php include "menu.php"; ?>
+        
+        <?php include "carousel.php"?>
+        
+        
+        <!-- start container-->
+        <div class="container">
+            
+            <!-- start very outer row-->
+            <div class="row row-content" >
+                <div class="col-xs-12 col-sm-10" id="outer-row">
+                    <!-- start first inner  row-->
+                    <div class="row row-content" id="history">
+                        <div class="col-xs-12 col-sm-12">
+                            <div class="media">
+                                <div class="media media-left media-middle">
+                                
+                                </div>
+                                <div class="media-body">
+                                    <h2 class="media-heading">History</h2>
+                                    <p>The university has a history that started in 1858 when Dhaka Brahma School was founded in 1858 by Dinanath Sen, Prabhaticharan Roy, Anathbandhu Mallik and Brajasundar Kaitra. The name Jagannath school was given by Kishorilal Chowdhury, the Zamindar of Baliati in Manikganj, who took over the school in 1872 and renamed it after his father. In 1884, it was raised to a second grade college. Law was one of the first courses introduced. A common management committee administered the school and college until 1887, when the school section was separated to form the independent Kishore Jubilee School. It is now known as K L Jubilee School. The administration of the college was transferred to a board of trustees in 1907. In the following year, it became a first grade college.
+
+The college started with 48 students and in five years, the roll raised to 396. In 1910, Raja Manmath Roy Chowdhury, the zamindar of Santosh, Tangail, affiliated the Pramath-Manmath College of Tangail with Jagannath College. With the establishment of University of Dhaka in 1921, it stopped admission to degree courses and was renamed Jagannath Intermediate College. This status was changed after 28 years in 1949, when it reopened degree classes. The college was taken over by the government in 1968.
+
+Jagannath College opened honours and masters programmes in 1975. That year the government once again took over the college and upgraded it into a postgraduate college. In 1982, the college closed its programmes at intermediate level. The college introduced evening shifts in 1992.
+
+It was transformed into Jagannath University in 2005 by passing a bill named Jagannath University Act-2005 in the national parliament.[3]</p>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+                    </div><!-- end frist inner row history-->  
+                    
+                    <div class="row row-content">
+                        <div class="col-sm-12 col-xs-12">
+                            <div class="media">
+                                <div class="media-right media-middle">
+                                    <h2 class="media-heading">Achivements</h2>
+                                </div>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-hover table-bordered">
+                                        <tr class="danger">
+                                            <th>&nbsp;</th>
+                                            <th>2009</th>
+                                            <th>2010</th>
+                                            <th>2011</th>
+                                            <th>2012</th>
+                                            <th>2013</th>
+                                            <th>2014</th>
+                                        </tr>
+                                        <tr class="success">
+                                            <th>Total Examinees</th>
+                                            <td>157</td>
+                                            <td>150</td>
+                                            <td>166</td>
+                                            <td>134</td>
+                                            <td>156</td>
+                                            <td>159</td>
+                                        </tr>
+                                        
+                                        <tr class="warning">
+                                            <th>Total passed</th>
+                                            <td>152</td>
+                                            <td>141</td>
+                                            <td>157</td>
+                                            <td>129</td>
+                                            <td>133</td>
+                                            <td>147</td>
+                                        </tr>
+                                        
+                                        <tr class="success">
+                                            <th>Total A+</th>
+                                            <td>40</td>
+                                            <td>37</td>
+                                            <td>52</td>
+                                            <td>30</td>
+                                            <td>42</td>
+                                            <td>45</td>
+                                        </tr>
+                                        
+                                        <tr class="warning">
+                                            <th>Total Golden A+</th>
+                                            <td>10</td>
+                                            <td>3</td>
+                                            <td>12</td>
+                                            <td>20</td>
+                                            <td>11</td>
+                                            <td>6</td>
+                                        </tr>
+                                        
+                                        <tr class="success">
+                                            <th>Passed (%)</th>
+                                            <td>96.82%</td>
+                                            <td>94.00%</td>
+                                            <td>94.58%</td>
+                                            <td>96.27%</td>
+                                            <td>85.25%</td>
+                                            <td>92.45%</td>
+                                        </tr>
+                                        
+                                        
+                                        
+                                        
+                                    </table>
+                                </div><!-- end table responsive class-->
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-2 col-sm-2">
+                
+                </div><!-- end right side panel-->
+            </div><!-- end very outer row-->
+        
+        </div>
+        <!--end main container-->
+        <?php include "footer.php";?>
+        
+        
+        
+        <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>                            
+        <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,28 @@
+<!--start nav bar-->
+            
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- start navbar container-->
+            <div class="container">
+                <div class="navbar-header">
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" aria-expanded="false" data-target="#navbar">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <li class="icon-bar"></li>
+                        <li class="icon-bar"></li>
+                        <li class="icon-bar"></li>
+                        <li class="icon-bar"></li>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="Mohangonj Pilot Govt. High School" width=100 height=30></a>
+                </div><!-- end navbar header-->
+                
+                <div class="navbar-collapse collapse" id="navbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li class=""><a href="#">Menu</a></li>
+                        <li class=""><a href="#">Contacts</a></li>
+                        <li class=""><a href="#">About</a></li>
+                        <li class=""><a href="#">Extra</a></li>
+                        
+                    </ul>
+                </div> <!-- end div for id navbar-->
+            </div><!-- end navbar container-->
+        </nav>
